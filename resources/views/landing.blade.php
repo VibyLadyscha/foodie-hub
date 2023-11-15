507cb91d764cf4a4554b50390adb57d739aadad7
VibyLadyscha <vibyladyscha@gmail.com>
2023-11-15 15:25:17 +0700

    Memasukkan file Laravel

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/landing.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <section class="hero">
+        <nav class="navbar">
+                <img src="image/F odie 1.png">
+              </a>
+              <ul>
+                <li><a href="#" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">About Us</a></li>
+                <li><a href="#" class="nav-link">Contact Us</a></li>
+                <li><a href="#" class="nav-link">Favorite</a></li>
+            </ul>
+            </div>
+          </nav>
+            <div class="images">
+                <img src="image/Telur_Balado_Recipe__Indonesian_Hard-Boiled_Eggs_in_Sambal_Balado_-removebg-preview 1.png" alt="Image 1" class="rotated1">
+                <img src="image/Ayam_Goreng_Kuning_Serundeng__by_Meat_Gong-removebg-preview 1.png" alt="Image 2" class="rotated2">
+                <img src="image/kepiting.png" alt="Image 3" class="rotated">
+            </div>
+            <div class="text-content">
+                <h2 class="sub-heading1">Whipping Up</h2>
+                <h1 class="heading">Happiness</h1>
+                <h2 class="sub-heading">on Every Plate!</h2>
+                <button class="cta-button">Pesan Sekarang!</button>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+
+</html>
